8e5da6a839bc92db51dd0b24b13050345f2d5c6b
Jason <jasonsawiris@gmail.com>
2025-01-30 19:31:11 -0800

    WPILIB Project

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,80 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+import java.lang.Math;
+import frc.robot.commands.TeleopDrive;
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Axis;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  private final Joystick portedXboxController = new Joystick(0);
+  private final int thrustAxis = XboxController.Axis.kRightTrigger.value;
+  private final int reverseAxis = XboxController.Axis.kLeftTrigger.value;
+  private final int steerAxisX = XboxController.Axis.kLeftX.value;
+  private final int shooter
+
+  public Drivetrain drive = new Drivetrain();
+
+  // private final MotorController shooterM = new PWMVictorSPX(5);
+
+  public RobotContainer() {
+    // Configure the trigger bindings
+    configureBindings();
+    drive.setDefaultCommand(
+        new TeleopDrive(drive,
+        () -> Constants.MotorConstants.motorLimitK*(portedXboxController.getRawAxis(steerAxisX)),
+        () -> Constants.MotorConstants.motorLimitK*(portedXboxController.getRawAxis(thrustAxis)-portedXboxController.getRawAxis(reverseAxis))));
+  }
+ 
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    // new Trigger(m_exampleSubsystem::exampleCondition)
+    // .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
+    // cancelling on release.
+    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    return null;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+  // Victor frontLeft;
+  // Victor frontRight;
+  // Victor backLeft;
+  // Victor backRight;
+  DifferentialDrive drive;
+  MotorControllerGroup leftMs;
+  MotorControllerGroup rightMs;
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    WPI_TalonSRX topLeftM = new WPI_TalonSRX(2);
+    WPI_VictorSPX topRightM = new WPI_VictorSPX(1);
+    WPI_VictorSPX bottomLeftM = new WPI_VictorSPX(3);
+    WPI_VictorSPX bottomRightM = new WPI_VictorSPX(4);
+
+    MotorControllerGroup leftMs = new MotorControllerGroup(topRightM, bottomRightM);
+    MotorControllerGroup rightMs = new MotorControllerGroup(topLeftM, bottomLeftM);
+    drive = new DifferentialDrive(leftMs, rightMs);
+    }
+
+  public void drive(double STEER, double SPEED) {
+    drive.arcadeDrive(STEER, SPEED);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
