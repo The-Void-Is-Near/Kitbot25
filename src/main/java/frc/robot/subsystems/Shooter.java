efc83133af87c43b313e78e182b058d77b3bfbed
JASON <jasonsawiris@gmail.com>
2025-02-17 17:07:35 -0800

    Added Comments

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import frc.robot.Constants;
-//sub systems do define how to run (thinking)
+//sub systems do define how to run (thinking) a chhange
 public class Shooter {
         TalonSRX shooterM;
         public Shooter(){
