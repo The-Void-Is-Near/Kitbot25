f7b1af7381ff6673d940c9bec872959b8bbe10ad
JASON <jasonsawiris@gmail.com>
2025-02-20 17:21:04 -0800

    Shooter

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,19 +1,27 @@
 
 package frc.robot;
+import edu.wpi.first.wpilibj.XboxController;
 
 public final class Constants {
   public static class Controls {
-    public static final int kDriverControllerPort = 0;
-    public static final double DriveSpeedLimit = 0.75;
-
+    public static final int ControllerPort = 0;
+    public static final int thrust = XboxController.Axis.kRightTrigger.value;
+    public static final int reverse = XboxController.Axis.kLeftTrigger.value;
+    public static final int steer = XboxController.Axis.kRightX.value;
+    public static final int shoot = XboxController.Button.kY.value;
+    public static final int shootReverse = XboxController.Button.kA.value;
   }
+public final class DriveBase{
+  public static final double MotorSpeedLimit = 0.75;
+
+}
   public static class Shooter{
     public static final double ShooterSpeedLimit = 1;
     public static final int ShooterMotorID = 6;
   }
+
   public static class Arm{
     public static final int ArmMotorID = 5;
-
   }
 }
  
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,46 +4,38 @@ import frc.robot.commands.TeleopShooter;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Shooter;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class RobotContainer {
-  private final Joystick portedController = new Joystick(0); //Set USB Port.
-  private final int thrustAxis = XboxController.Axis.kRightTrigger.value; //ID of Axis. (axis is analog, but this is not data)
-  private final int reverseAxis = XboxController.Axis.kLeftTrigger.value;
-  private final int steerAxisX = XboxController.Axis.kRightX.value;
-  private JoystickButton shooterButton = new JoystickButton(portedController, XboxController.Button.kY.value);//Reads and sets to real value (BOOL) via the ID.
-  private JoystickButton shooterButtonReverse = new JoystickButton(portedController, XboxController.Button.kA.value);//Reads and sets to real value (BOOL) via the ID.
-  
-  //private POVButton algearmButton = new POVButton(portedController, 0);//Reads and sets to real value (BOOL) via the ID.
-  //private POVButton algearmButtonReverse = new POVButton(portedController, 180);//Reads and sets to real value (BOOL) via the ID.
+  private final Joystick portedController = new Joystick(Constants.Controls.ControllerPort); 
 
-  public Drivetrain drive = new Drivetrain();//Used class in subsystem and creates an object.
-  public Shooter shooter = new Shooter();// The COMMANDS call the SUBSYSTEM
+  private final int thrustAxis = Constants.Controls.thrust;
+  private final int reverseAxis = Constants.Controls.reverse;
+  private final int steerAxisX = Constants.Controls.steer;
+
+  private JoystickButton shooterButton = new JoystickButton(portedController, Constants.Controls.shoot);
+  private JoystickButton shooterButtonReverse = new JoystickButton(portedController, Constants.Controls.shootReverse);
+
+  public Drivetrain drive = new Drivetrain();
+  public Shooter shooter = new Shooter();
 
   public RobotContainer() {
-    configureBindings();//Defines controls (Xbox)
+    configureBindings();
     drive.setDefaultCommand(
         new TeleopDrive(drive,
-        () -> Constants.MotorConstants.motorLimitK*(portedController.getRawAxis(steerAxisX)),
-        () -> Constants.MotorConstants.motorLimitK*(portedController.getRawAxis(thrustAxis)-portedController.getRawAxis(reverseAxis))));//Flip Thrust
+        () -> Constants.DriveBase.MotorSpeedLimit*(portedController.getRawAxis(steerAxisX)),
+        () -> Constants.DriveBase.MotorSpeedLimit*(portedController.getRawAxis(thrustAxis)-portedController.getRawAxis(reverseAxis))));
   }
   
   private void configureBindings() {
-    shooterButton.whileTrue(new TeleopShooter(shooter, Constants.MotorConstants.shooterK));
-    shooterButtonReverse.whileTrue(new TeleopShooter(shooter, -Constants.MotorConstants.shooterK));
+    shooterButton.whileTrue(new TeleopShooter(shooter, Constants.Shooter.ShooterSpeedLimit));
+    shooterButtonReverse.whileTrue(new TeleopShooter(shooter, -Constants.Shooter.ShooterSpeedLimit));
     shooterButton.onFalse(new TeleopShooter(shooter, 0.0));
     shooterButtonReverse.onFalse(new TeleopShooter(shooter, 0.0));
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
     return null;
   }
 }
